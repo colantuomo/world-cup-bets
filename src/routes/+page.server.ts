84284b2caa0b8e59dce1873f42bea7a274c9814c
colantuomo <pcolantuomo@gmail.com>
2022-11-18 19:34:57 -0300

    fix: using onMount

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,10 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad, PageData } from './$types';
+import type { PageServerLoad } from './$types';
 import { getMatchesWithBets } from '../services';
 
 // it so that it gets served as a static asset in production
 export const prerender = true;
 
-export const load: PageServerLoad = async ({ locals }: PageData) => {
-	if (!locals?.userId) {
-		throw redirect(302, '/login');
-	}
+export const load: PageServerLoad = async () => {
 	const matches = await getMatchesWithBets();
 	return {
 		matches
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import type { MatchesData } from '../types';
 	import Card from '../components/card.svelte';
 	import { initializeFirebase } from '../lib/firebase';
@@ -7,8 +8,15 @@
 	const { auth } = initializeFirebase();
 
 	let currentUser: any;
-	auth.onAuthStateChanged((user) => {
-		currentUser = user;
+
+	onMount(() => {
+		auth.onAuthStateChanged((user) => {
+			console.log('auth state changed', user);
+			if (user === null) {
+				window.location.href = '/login';
+			}
+			currentUser = user;
+		});
 	});
 </script>
 
@@ -17,11 +25,17 @@
 	<meta name="description" content="World Cup Bets App" />
 </svelte:head>
 
+{#if currentUser}
+	logged as {currentUser.displayName}
+{/if}
+
 <section class="container mx-auto px-4">
 	<h1 class="text-center py-4">Daniel do Bets</h1>
 	<div class="flex flex-col gap-5">
-		{#each data.matches as match}
-			<Card />
-		{/each}
+		{#if data?.matches.length > 0}
+			{#each data.matches as match}
+				<Card />
+			{/each}
+		{/if}
 	</div>
 </section>

7db74be4c47af5a1230897244f32b80111d274d5
colantuomo <pcolantuomo@gmail.com>
2022-11-18 19:39:07 -0300

    fix: remmoving service

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
 import type { PageServerLoad } from './$types';
-import { getMatchesWithBets } from '../services';
+// import { getMatchesWithBets } from '../services';
 
 // it so that it gets served as a static asset in production
 export const prerender = true;
 
 export const load: PageServerLoad = async () => {
-	const matches = await getMatchesWithBets();
+	const matches = {};
 	return {
 		matches
 	};

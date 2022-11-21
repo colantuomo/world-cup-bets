aeb09c8e273c78e59786c0b2814e330ea1e85cbe
Paulo <18310096+colantuomo@users.noreply.github.com>
2022-11-21 15:03:34 -0300

    feat: creating a componete for button

---
+++ src/components/button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { Moon } from 'svelte-loading-spinners';
+
+	const dispatch = createEventDispatcher();
+	export let loading: boolean;
+	export let text: string;
+
+	function onClick() {
+		dispatch('onPress', {});
+	}
+</script>
+
+<button
+	disabled={loading}
+	class="flex justify-center bg-blue-500 text-white p-2 rounded-lg font-bold w-52 disabled:bg-gray-300"
+	on:click={onClick}
+>
+	{#if loading}
+		<Moon size={24} color="black" />
+	{:else}
+		{text}
+	{/if}
+</button>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,30 +1,33 @@
-<script>
+<script lang="ts">
 	import { initializeFirebase } from '$lib/firebase';
 	import logo from '$lib/images/catar-logo.png';
+	import Button from '../../components/button.svelte';
 	import { createUser, getUser } from '../../services';
 	const { signInWithPopup, auth, googleProvider } = initializeFirebase();
+	let loading: boolean;
 
 	function login() {
-		signInWithPopup(auth, googleProvider).then(async ({ user }) => {
-			document.cookie = `userId=${user.uid}`;
-			const { error } = await getUser(user.uid);
-			if (error) {
-				const { error } = await createUser({
-					googleUserId: user.uid,
-					name: user.displayName ?? '',
-					avatar: null
-				});
-				if (error === null) window.location.href = '/';
-				return;
-			}
-			window.location.href = '/';
-		});
+		loading = true;
+		signInWithPopup(auth, googleProvider)
+			.then(async ({ user }) => {
+				document.cookie = `userId=${user.uid}`;
+				const { error } = await getUser(user.uid);
+				if (error) {
+					const { error } = await createUser({
+						googleUserId: user.uid,
+						name: user.displayName ?? '',
+						avatar: null
+					});
+					if (error === null) window.location.href = '/';
+					return;
+				}
+				window.location.href = '/';
+			})
+			.finally(() => (loading = false));
 	}
 </script>
 
 <div class="container mx-auto px-6 md:px-60 flex flex-col items-center">
 	<img class="w-96" alt="The project logo" src={logo} />
-	<button class="bg-blue-500 text-white p-2 rounded-lg font-bold w-52" on:click={login}
-		>Login with google</button
-	>
+	<Button on:onPress={login} {loading} text="Login with google" />
 </div>

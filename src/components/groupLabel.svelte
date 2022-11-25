070cffb2a0f63a52bdad94402fe8b0d9464136ab
colantuomo <pcolantuomo@gmail.com>
2022-11-25 20:07:01 -0300

    fix: new and old bets request

--- src/components/groupLabel.svelte
+++ src/components/groupLabel.svelte
@@ -20,8 +20,8 @@
 	async function sendBets() {
 		isLoading = true;
 		try {
-			const oldBets = bets.filter(({ id }) => id !== undefined);
-			const newBets = bets.filter(({ id }) => id === undefined);
+			const oldBets = bets.filter(({ id }) => id !== null);
+			const newBets = bets.filter(({ id }) => id === null);
 			if (newBets.length > 0) {
 				await saveBets(currentUser.userId, bets);
 			}

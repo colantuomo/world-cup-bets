2077e96a51b642695eae95bc3dad6d3f411ac27a
Paulo <18310096+colantuomo@users.noreply.github.com>
2022-11-25 16:22:06 -0300

    fix: pre render fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,11 +2,10 @@ import type { ServerLoadEvent } from '@sveltejs/kit';
 import { getMatchesWithBets } from '../services';
 import type { CustomLocals, MatchesData, PageLoadData } from '../types';
 
+export const prerender = true;
 interface CustomServerLoadEvent extends ServerLoadEvent {
 	locals: CustomLocals;
 }
-
-export const prerender = true;
 
 export function load({ locals }: CustomServerLoadEvent): PageLoadData<Promise<MatchesData>> {
 	const response = getMatchesWithBets(locals.userId);

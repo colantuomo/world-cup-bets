83260059d0bfa81359c8adbfe12ea6713e683d8e
colantuomo <pcolantuomo@gmail.com>
2022-11-25 18:42:51 -0300

    fix: using name from session

--- src/routes/matches/[id]/+page.svelte
+++ src/routes/matches/[id]/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { initializeFirebase } from '$lib/firebase';
 	import { fade } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
 	import GroupLabel from '../../../components/groupLabel.svelte';
 	import { Groups, type GroupIndex, type MatchesData, type PageLoadData } from '../../../types';
-	const { auth } = initializeFirebase();
 
 	export let data: PageLoadData<MatchesData>;
 	const matches = data.response;
+	const userName = sessionStorage.getItem('rankingUserName');
 
 	function getNameByGroupIndex(index: string) {
 		return Groups[index as GroupIndex].toString();
@@ -23,7 +22,7 @@
 	<div class="bg-yellow-400 h-20 sticky top-0 flex gap-1 justify-center items-center flex-col z-20">
 		<div class="flex gap-2 text-sm md:text-base">
 			<p>Você está vendo as apostas de:</p>
-			<p class="font-bold">{data.locals.userName}</p>
+			<p class="font-bold">{userName ?? ''}</p>
 		</div>
 		<div class="flex gap-4 text-sm underline font-semibold">
 			<button on:click={() => (window.location.href = '/')}>Minhas apostas</button>
